e53e0703e5e504a0af66edd752049833f7130114
dezlymacauley <dezlymacauley@proton.me>
2026-02-16 16:40:48 +0200

    Added more notes on floats

--- 04-stack-allocated-primitive-types/topic-04-double/src/main.cpp
+++ 04-stack-allocated-primitive-types/topic-04-double/src/main.cpp
@@ -1,26 +1,32 @@
 /*
 
-    ABOUT: float
+    ABOUT: double
 
-    The `float` data type is for storing number that have a decimal part.
+    The `double` data type is for storing number that have a decimal part.
     These numbers can be positive or negative.
 
-    Like the `int` data type, the `float` data type is also 32 bits (4 bytes)
+    The `double` is 64 bits (8 bytes) and offers more precision than the
+    `float` data type.
 
-    IEEE-754 float standard guarantees at least 6 decimal digits of precision.
+    IEEE-754 double standard guarantees at least 15 decimal
+    digits of precision.
+
+    Anything decimals after 15 digits may not be accurately stored
+    due to rounding.
 
     That means that it is good for storing numbers where you only care about
     the first 6 decimal being accurately stored.
 
-    Anything beyond 6 digits may or may not be exact due to rounding.
+    This makes `double` suitable for calculations where a higher precision
+    is required.
 
 */
 
 #include <print>
 
 // TIP: You can add the `cfloat` header file to have access to values
-// like `-FLT_MIN` that can be used to view the smallest number that
-// a float can store.
+// like `-DBL_MIN` that can be used to view the smallest number that
+// a double can store.
 #include <cfloat>
 
 using std::println;
@@ -34,7 +40,8 @@ int main() {
     // and not a `double` data type (I will have a separate guide on this
     // data type)
 
-    float interest_rate{12.62f};
+    // Don't add the 'f' suffix at the end when using double
+    double interest_rate{12.62f};
     println("interest_rate: {}", interest_rate);
     // interest_rate: 12.62
 
@@ -49,11 +56,11 @@ int main() {
 
     // SECTION: How to print the storage range of the int type
 
-    println("The most negative number that a float can store: {}", -FLT_MIN);
-    // The most negative number that a float can store: -1.1754944e-38
+    println("The most negative number a double can store: {}", -DBL_MIN);
+    // The most negative number a double can store: -2.2250738585072014e-308
 
-    println("The most positive number that a float can store: {}", FLT_MAX);
-    // The most positive number that a float can store: 3.4028235e+38
+    println("The most positive that a double can store: {}", DBL_MAX);
+    // The most positive that a double can store: 1.7976931348623157e+308
 
     // The format of the output is called `exponential notation`
     // or `scientific` notation. This is used to display very small or very
