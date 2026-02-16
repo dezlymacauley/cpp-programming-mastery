c21ab80d960f412bdb0b6e71f919e4d0e736ff77
dezlymacauley <dezlymacauley@proton.me>
2026-02-16 12:41:23 +0200

    New lessons added

---
+++ 02-making-comments/topic-01-single-line-comments/src/main.cpp
@@ -0,0 +1,11 @@
+// This is a single line comment
+
+/*
+    This is a multi-line comment.
+    Everything between the two asterisks `*`, will be treated as a comment. 
+*/
+
+int main() {
+
+    return 0;
+}
---
+++ 03-standard-output/topic-01-cout/src/main.cpp
@@ -0,0 +1,13 @@
+// This includes a header file called "iostream"
+#include <iostream>
+
+// This brings `cout` from `std` (the standard library) into scope.
+// This allows you to use `cout` directly in this C++ file,
+// rather than having to type out `std::cout` whenever you want to use it.
+using std::cout;
+
+int main() {
+
+    cout << "C++ Programming Mastery\n";
+    return 0;
+}
