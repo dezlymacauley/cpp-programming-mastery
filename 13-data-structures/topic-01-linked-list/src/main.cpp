85a51c58bd1c88a26121b545002b0f2f63e6dc11
dezlymacauley <dezlymacauley@proton.me>
2026-02-20 16:29:24 +0200

    Started with Linked Lists

---
+++ 13-data-structures/topic-01-linked-list/src/main.cpp
@@ -0,0 +1,22 @@
+/*
+
+    ABOUT: Linked List
+
+    Unlike a `vector`, you can't target each element in a Linked List 
+    by using an index. This is because the elements are not arranged 
+    continuously in memory.
+
+    It's a list made of nodes.
+    1. A Linked List, is a list of nodes
+    2. Each node contains a value and a pointer to the next node on the list.
+    3. The last node contains a value and a nullptr to indicate 
+    that it is the end of the list.
+
+*/
+
+#include <print>
+using std::println;
+
+int main() {
+
+}
